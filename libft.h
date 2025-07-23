c9aa1477bb0d0545984076ba92ac22d1712cbde4
Henrique Maciel Dos Reis <hemaciel@c3r1p3.42sp.org.br>
2025-07-23 20:55:01 -0300

    day3a

--- libft.h
+++ libft.h
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hemaciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/22 21:20:08 by hemaciel          #+#    #+#             */
-/*   Updated: 2025/07/23 20:52:21 by hemaciel         ###   ########.fr       */
+/*   Created: 2025/07/23 19:39:14 by hemaciel          #+#    #+#             */
+/*   Updated: 2025/07/23 20:22:48 by hemaciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <libft.h>
-void	*ft_memset(void *s, int c, size_t n)
-{
-	char	*p;
 
-	p = (char *)s;
-	while(n--)
-	{
-		*p++ = (char)c;
-	}
-	return (s);
-}
+#ifndef	LIBFT_H
+#define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+void	*ft_memset(void *s, int c, size_t n);
+#endif
