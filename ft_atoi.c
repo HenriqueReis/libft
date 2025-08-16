3037506b7265a09f70018fa56c06a64aced46876
Henrique Maciel Dos Reis <hemaciel@c2r7p1.42sp.org.br>
2025-08-16 13:38:55 -0300

    atoi

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hemaciel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 12:08:31 by hemaciel          #+#    #+#             */
+/*   Updated: 2025/08/16 13:34:50 by hemaciel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	nbr;
+	int	isnegative;
+
+	i = 0;
+	nbr = 0;
+	isnegative = 0;
+	while (nptr[i] == 32 || (nptr[i] > 8 && nptr[i] < 14))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			isnegative = 1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{	
+		nbr = (nbr * 10) + (nptr[i] - '0');
+		i++;
+	}
+	if (isnegative)
+		return (-nbr);
+	return (nbr);
+}
