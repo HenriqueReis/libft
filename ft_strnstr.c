595002d23fbf3b3c903b9bda1a6e71fb70375814
Henrique Maciel Dos Reis <hemaciel@c2r7p1.42sp.org.br>
2025-08-16 12:41:12 -0300

    strlcat strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hemaciel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 11:05:14 by hemaciel          #+#    #+#             */
+/*   Updated: 2025/08/16 12:07:34 by hemaciel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	littlelen;
+
+	i = 0;
+	littlelen = ft_strlen(little);
+
+	if (!ft_strlen(little))
+		return ((char *)big);
+	while (big[i] != '\0' && (i + littlelen <= len))
+	{
+		if (big[i] == little[0])
+		{
+			j = 0;
+			while (big[j+i] != '\0' && little[j] !='\0')
+			{
+				if (big[i + j] != little[j])
+					break;
+				else if (j == littlelen - 1)
+					return ((char *) big + i);
+				j++;
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hemaciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 19:39:14 by hemaciel          #+#    #+#             */
-/*   Updated: 2025/08/09 17:26:50 by hemaciel         ###   ########.fr       */
+/*   Updated: 2025/08/16 12:12:07 by hemaciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int	ft_isprint(int c);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_atoi(const char *nptr);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -35,4 +36,5 @@ size_t	ft_strlen(const char *c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 #endif
